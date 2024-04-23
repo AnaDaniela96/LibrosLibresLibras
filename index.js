57deae5809e15f17ec6dfd79712d56434c5b73b6
AnaDaniela96 <108939830+AnaDaniela96@users.noreply.github.com>
2024-04-23 15:54:39 -0600

    Update index.js

--- index.js
+++ index.js
@@ -10,4 +10,34 @@ document.getElementById('myForm').addEventListener('submit', function(event) {
     console.log("Nombre: " + nombre);
     console.log("Correo Electrónico: " + correo);
     console.log("Aceptó aviso de privacidad: " + avisoPrivacidad);
+
+    // Construir el objeto de datos para enviar a Mailchimp
+    // var data = {
+    //     email_address: correo,
+    //     status: 'subscribed',
+    //     merge_fields: {
+    //         FNAME: nombre
+    //     }
+    // };
+
+    // Convertir el objeto de datos a JSON
+    // var jsonData = JSON.stringify(data);
+
+    // Hacer la solicitud a la API de Mailchimp
+    // fetch('https://<DC>.api.mailchimp.com/3.0/lists/<LIST_ID>/members', {
+    //     method: 'POST',
+    //     headers: {
+    //         'Authorization': 'Basic ' + btoa('anystring:' + '111c48a1c77b8186db7732a5e788c523-us18'),
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: jsonData
+    // })
+    // .then(response => {
+    //     if (response.ok) {
+    //         console.log('¡Suscripción exitosa!');
+    //     } else {
+    //         console.error('Error al suscribirse:', response.statusText);
+    //     }
+    // })
+    // .catch(error => console.error('Error en la solicitud:', error));
 });
